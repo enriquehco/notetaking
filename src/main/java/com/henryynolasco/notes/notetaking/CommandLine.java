c1905b3dfc20f9a43ec2b98c7d495754a3bd6380
Hernandez Carrillo Enrique <enrique.hernandezcarrillo@solera.com>
2022-10-19 10:07:47 +0200

    Simple command line input

---
+++ src/main/java/com/henryynolasco/notes/notetaking/CommandLine.java
@@ -0,0 +1,16 @@
+package com.henryynolasco.notes.notetaking;
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLine implements CommandLineRunner {
+	@Override
+    public void run(String... args) throws Exception {
+        System.out.println("Enter word!");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+     }
+}
--- src/main/java/com/henryynolasco/notes/notetaking/Note.java
+++ src/main/java/com/henryynolasco/notes/notetaking/Note.java
@@ -1,5 +1,85 @@
 package com.henryynolasco.notes.notetaking;
 
 public class Note {
-
+	private int id;
+	private String description;
+	private String text;
+	private String datecreation;
+	private String estimateCompletion;
+	private String completion;
+	private String link;
+	private String mentions;
+	private int priority;
+	
+	
+	
+	public Note(int id, String description, String text, String datecreation, String estimateCompletion,
+			String completion, String link, String mentions, int priority) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.text = text;
+		this.datecreation = datecreation;
+		this.estimateCompletion = estimateCompletion;
+		this.completion = completion;
+		this.link = link;
+		this.mentions = mentions;
+		this.priority = priority;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getDatecreation() {
+		return datecreation;
+	}
+	public void setDatecreation(String datecreation) {
+		this.datecreation = datecreation;
+	}
+	public String getEstimateCompletion() {
+		return estimateCompletion;
+	}
+	public void setEstimateCompletion(String estimateCompletion) {
+		this.estimateCompletion = estimateCompletion;
+	}
+	public String getCompletion() {
+		return completion;
+	}
+	public void setCompletion(String completion) {
+		this.completion = completion;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getMentions() {
+		return mentions;
+	}
+	public void setMentions(String mentions) {
+		this.mentions = mentions;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+	
 }
